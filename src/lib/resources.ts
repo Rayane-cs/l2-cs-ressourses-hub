755c2b18ce750ef072ba5156882820eb049be9c9
Youbi <kessoularayane@gmail.com>
2025-11-01 20:44:32 +0100

    Add New Files
    Add the course file of  session one of english

--- src/lib/resources.ts
+++ src/lib/resources.ts
@@ -318,6 +318,15 @@ const resources: Record<string, Array<{ id: string; title: string; type: string;
       type: "tp",
       driveUrl: "https://drive.google.com/file/d/1ZZMpStfXGyHsvJKNJmyKylkOTDXKaif5/view?usp=drive_link",
     },
+  ],
+  english: [
+    {
+      id: "course",
+      title: "Session 01",
+      semester: "S3",
+      type: "course",
+      driveUrl: "https://docs.google.com/document/d/1jVLj-gbeshjjKzSl70KT3vsH9gh_3zDB/edit?usp=sharing&ouid=114862124461485137731&rtpof=true&sd=true",
+    }
   ]
 };
 
