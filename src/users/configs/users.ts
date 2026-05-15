641a255759b38846fdbde6da28134327f642798a
ryn <kessoularayaneayoub@gmail.com>
2026-05-15 11:32:58 +0100

    Update users personalized info

--- src/users/configs/users.ts
+++ src/users/configs/users.ts
@@ -73,396 +73,577 @@ const ayoub: UserConfig = {
 const azzouz_abdesattar: UserConfig = {
   id: "d2257c52-69f0-490a-bbb5-6c19f10c722f",
   displayName: "Abdesattar",
+
+  showOnlyYears: ["l2"],
 };
 
 const lazreg_nihad: UserConfig = {
   id: "484c2866-3e30-4df0-a68c-2efe42a34a73",
   displayName: "Nihad",
+  welcomeMessage: "Welcome back, Nihad! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const roro_1: UserConfig = {
   id: "ed030548-526e-4837-8027-ab78bbead78d",
   displayName: "Roro",
+  welcomeMessage: "Welcome back, Roro! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const mohamed_amine: UserConfig = {
   id: "8aa26697-599d-40df-b67e-6cc94143d08b",
   displayName: "Amine",
+  welcomeMessage: "Welcome back, Amine! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const reggoua_insaf: UserConfig = {
   id: "4c717533-1103-42f2-9770-95e89cdaa0e4",
   displayName: "Insaf",
+  welcomeMessage: "Welcome back, Insaf! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const basmala: UserConfig = {
   id: "58d44998-cd71-4cea-aadf-af80bd13ced5",
   displayName: "Basmala",
+  welcomeMessage: "Welcome back, Basmala! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const basmala_2: UserConfig = {
   id: "a723a9cd-cbc8-4262-be64-66d59658a185",
   displayName: "Basmala",
+  welcomeMessage: "Welcome back, Basmala! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const bouthaina_atsaoui: UserConfig = {
   id: "6aa75e82-1d91-4052-8efb-43134c1c3c89",
   displayName: "Bouthaina",
+  welcomeMessage: "Welcome back, Bouthaina! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const hadjer: UserConfig = {
   id: "b512673b-7a53-4aa5-a881-f03e414c4b97",
   displayName: "Hadjer",
+  welcomeMessage: "Welcome back, Hadjer! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const fatima_beghdaoui: UserConfig = {
   id: "ac1d1623-60a1-4bda-a3c5-e09602fef181",
   displayName: "Fatima",
+  welcomeMessage: "Welcome back, Fatima! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const fatima_beghdaoui_2: UserConfig = {
   id: "8b5c96ba-70dd-4a0a-a881-0d931eb1de43",
   displayName: "Fatima",
+  welcomeMessage: "Welcome back, Fatima! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const amina_beliche: UserConfig = {
   id: "410f824c-f89b-426f-a186-491a40ff4123",
   displayName: "Amina",
+  welcomeMessage: "Welcome back, Amina! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const abdelkayoum_nouri: UserConfig = {
   id: "39ecc778-0945-4740-9814-7a53494a4dfc",
   displayName: "Nouri",
+  welcomeMessage: "Welcome back, Nouri! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const houda_ahmedi: UserConfig = {
   id: "910496a9-88b3-4551-905f-228cea130356",
   displayName: "Houda",
+  welcomeMessage: "Welcome back, Houda! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const aria_armine: UserConfig = {
   id: "321adc3c-0252-4431-9d93-4f9ebffbdfc9",
   displayName: "Armine",
+  welcomeMessage: "Welcome back, Armine! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const mahdi_mohammed: UserConfig = {
   id: "8d999abb-3871-4112-b41f-4567c9c06c81",
   displayName: "Mahdi",
+  welcomeMessage: "Welcome back, Mahdi! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const gribi: UserConfig = {
   id: "4cae6815-92d4-4e3e-9fa4-cc0d47febf09",
   displayName: "Gribi",
+  welcomeMessage: "Welcome back, Gribi! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ikhlas_1: UserConfig = {
   id: "e1747468-f47e-4466-a8ef-49ef78b0a432",
   displayName: "Ikhlas",
+  welcomeMessage: "Welcome back, Ikhlas! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ekim: UserConfig = {
   id: "fe340b6b-f8d0-44ee-b31e-65110029ae73",
   displayName: "Ekim",
+  welcomeMessage: "Welcome back, Ekim! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const terchi_khadidja: UserConfig = {
   id: "63b3c63e-c85d-4166-8271-3fd9ac919c97",
   displayName: "Khadidja",
+  welcomeMessage: "Welcome back, Khadidja! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const no_data_bro: UserConfig = {
   id: "8b2b247c-07ba-4f53-8045-6c2caf01e37e",
   displayName: "No data",
+  welcomeMessage: "Welcome back! Ready to study? (But no data for you :D)",
+  showOnlyYears: ["l2"],
+
+  customBanner: "Zyer rohek, w 9ra mlih No data w manich bro hhh",
 };
 
 const maro: UserConfig = {
   id: "03b581a1-284a-4ab1-8d09-1da364e90ad3",
   displayName: "Maro",
+  welcomeMessage: "Welcome back, Maro! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const agrouche_abdelbaki: UserConfig = {
   id: "b16c258a-0cf8-4235-ab4d-267c37548aff",
   displayName: "Abdelbaki",
+  welcomeMessage: "Welcome back, Abdelbaki! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const amin_mehdaoui: UserConfig = {
   id: "04cd401a-94c4-41d3-a07f-66536a8debab",
   displayName: "Amin",
+  welcomeMessage: "Welcome back, Amin! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const nasrallah_abbad: UserConfig = {
   id: "cbb52f5d-1cb0-4e58-ae52-0ccd780b23d1",
   displayName: "Nasrallah",
+  welcomeMessage: "Welcome back, Nasrallah! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const yass: UserConfig = {
   id: "2a236a8e-a1a8-484d-9200-564160644bf0",
   displayName: "Yass",
+  welcomeMessage: "Welcome back, Yass! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ikhlas_2: UserConfig = {
   id: "372306d4-5dbc-41bd-abbb-a561b34a5bf6",
   displayName: "Ikhlas",
+  welcomeMessage: "Welcome back, Ikhlas! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const smth: UserConfig = {
   id: "74adfa12-e169-4665-8998-fa2a5bbccf85",
   displayName: "Manar",
+  welcomeMessage: "Welcome back, Manar! Ready to study?",
+  showOnlyYears: ["l2"],
+
+  customBanner: "Study hard, to avoid ratrapage",
 };
 
 const roro_2: UserConfig = {
   id: "ed7af091-c445-4d55-9239-a8869e7270ba",
   displayName: "Roro",
+  welcomeMessage: "Welcome back, Roro! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const bahaa: UserConfig = {
   id: "7a451f67-c4a8-4c3a-92c1-b123a1cca411",
   displayName: "Bahaa",
+  welcomeMessage: "Welcome back, Bahaa! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const zahira_tlb: UserConfig = {
   id: "fd5daf16-6924-48f5-ac1a-52cfc6305c25",
   displayName: "Zahira",
+  welcomeMessage: "Welcome back, Zahira! Ready to study?",
+
+  showOnlyYears: ["l2"],
+  customBanner: "Zyeri rohek, w 9ray mlih bah matdo5lich ratrapage",
 };
 
 const toufik: UserConfig = {
   id: "87cbab4b-6d79-435f-b158-259666adf379",
   displayName: "Toufik",
+  welcomeMessage: "Welcome back, Toufik! Ready to study?",
+
+  showOnlyYears: ["l2"],
+  customBanner: "Zyer rohek, w 9ra mlih",
 };
 
 const hamza_bellabaci: UserConfig = {
   id: "64c4c541-fa5a-4b34-a531-c66b7253a06b",
   displayName: "Hamza",
+  welcomeMessage: "Welcome back, Hamza! Ready to study?",
+
+  showOnlyYears: ["l2"],
 };
 
 const bouba: UserConfig = {
   id: "2a21d226-98cd-4586-90b7-ad053e6fcf9b",
   displayName: "Bouba",
+  welcomeMessage: "Welcome back, Bouba! Ready to study?",
+
+  showOnlyYears: ["l2"],
 };
 
 const kobzili_raounek: UserConfig = {
   id: "7659f8c6-a5b0-4b69-ba2c-2124cf18b143",
   displayName: "Raounek",
+  welcomeMessage: "Welcome back, Raounek! Ready to study?",
+
+  showOnlyYears: ["l2"],
 };
 
 const meknaci_khadidja: UserConfig = {
   id: "5be7cb34-479b-4c33-838e-cd8bfdc63095",
   displayName: "Khadidja",
+  welcomeMessage: "Welcome back, Khadidja! Ready to study?",
+
+  showOnlyYears: ["l2"],
 };
 
 const mehali_wafa: UserConfig = {
   id: "90f35c83-ccd4-4e1e-b9fd-6b47c83517c5",
   displayName: "Wafa",
+  welcomeMessage: "Welcome back, Wafa! Ready to study?",
+
+  showOnlyYears: ["l2"],
 };
 
 const milisa_chife: UserConfig = {
   id: "97344c75-fdfa-4d0b-9efa-7f64f2adc031",
   displayName: "Milisa",
+  welcomeMessage: "Welcome back, Milisa! Ready to study?",
+
+  showOnlyYears: ["l2"],
 };
 
 const douaa: UserConfig = {
   id: "8335d28b-f609-41ff-8538-887c071c269d",
   displayName: "Douaa",
+  welcomeMessage: "Welcome back, Douaa! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const karima_krelifaoui: UserConfig = {
   id: "6aff1d7d-8570-43c9-a675-acaac909a88f",
   displayName: "Karima",
+  welcomeMessage: "Welcome back, Karima! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const alihaimoud_saddik: UserConfig = {
   id: "f0f76ecd-c627-4130-bd6d-98325c9b7fdc",
   displayName: "Saddik",
+  welcomeMessage: "Welcome back, Saddik! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const chaimaa: UserConfig = {
   id: "be3780ff-9c0f-409c-ac8f-eaf23b4e765a",
   displayName: "Chaimaa",
+  welcomeMessage: "Welcome back, Chaimaa! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const a: UserConfig = {
   id: "7f37d4d3-7360-4a37-b2c7-20b39d401df1",
   displayName: "A",
+  welcomeMessage: "Welcome back, A! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const chikh_halima: UserConfig = {
   id: "583943c9-2977-4a90-b5f3-37f89b099189",
   displayName: "Halima",
+  welcomeMessage: "Welcome back, Halima! Ready to study?",
+  customBanner: "Awdi aw mdli esmk :)",
+  showOnlyYears: ["l2"],
 };
 
 const khhaldi_mounir: UserConfig = {
   id: "d28013c2-d670-401f-98e4-d0018999fa51",
   displayName: "Mounir",
+  welcomeMessage: "Welcome back, Mounir! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ziani_abdo: UserConfig = {
   id: "a003a50e-6931-49a5-9ff6-8c4b9ded3cfb",
   displayName: "Abdo",
+  welcomeMessage: "Welcome back, Abdo! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const berkani_chahinez: UserConfig = {
   id: "d1d57763-4ef7-405f-8630-7456993cb7a3",
   displayName: "Chahinez",
+  welcomeMessage: "Welcome back, Chahinez! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const yasser_aiad: UserConfig = {
   id: "33d09d10-924d-4491-8e52-ac7b460c43a6",
   displayName: "Yasser",
+  welcomeMessage: "Welcome back, Yasser! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const romaissa_zg: UserConfig = {
   id: "c2d776a5-201c-40e5-9027-7f69df8691c4",
   displayName: "Romaissa",
+  welcomeMessage: "Welcome back, Romaissa! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const neight_rahil: UserConfig = {
   id: "2dabb6df-5d8c-4b8a-9bbb-5096425279b7",
   displayName: "Rahil",
+  welcomeMessage: "Welcome back, Rahil! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const chaimaa_b0ba: UserConfig = {
   id: "b0ba59a1-9e07-45b3-9729-066273bf49eb",
   displayName: "Chaima",
+  welcomeMessage: "Welcome back, Chaima! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const chaimaa_2: UserConfig = {
   id: "de7cb8cd-38f6-4e86-9c6e-ef173269991e",
   displayName: "Chaima",
+  welcomeMessage: "Welcome back, Chaima! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ab_marwa: UserConfig = {
   id: "5e304f22-2bae-43a3-a1eb-a1ac20c45b2d",
   displayName: "Marwa",
+  welcomeMessage: "Welcome back, Marwa! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const nihad: UserConfig = {
   id: "9eedc16c-2e66-4768-80d6-cef813ea72c9",
   displayName: "Nihad",
+  welcomeMessage: "Welcome back, Nihad! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const benotsmane_youssuf: UserConfig = {
   id: "8563e6e3-89aa-4408-9784-402bcc31bda3",
   displayName: "Youssuf",
+  welcomeMessage: "Welcome back, Youssuf! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const yahiaoui_souhaibe: UserConfig = {
   id: "b16a23eb-9ad6-457e-a139-e85a5800a345",
   displayName: "Souhaibe",
+  welcomeMessage: "Welcome back, Souhaibe! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const fedj_manel: UserConfig = {
   id: "fe7449b0-2c16-4f6f-9290-5eae7ac8dd79",
   displayName: "Manel",
+  welcomeMessage: "Welcome back, Manel! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const moumen_hb: UserConfig = {
   id: "c736e50c-95bd-4a91-bbab-8682d09b3f49",
   displayName: "Moumen",
+  welcomeMessage: "Welcome back, Moumen! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const siali_basma: UserConfig = {
   id: "c8a23f24-e71c-44f7-9988-ad46ccff96b2",
   displayName: "Basma",
+  welcomeMessage: "Welcome back, Basma! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const gahlam_youcef: UserConfig = {
   id: "5cce450b-20cf-4c99-81e2-8a3acb620118",
   displayName: "Youcef",
+  welcomeMessage: "Welcome back, Youcef! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ines_elagrari: UserConfig = {
   id: "c616f7e6-1a91-426a-8af5-cf2e0f00d91a",
   displayName: "Inas",
+  welcomeMessage: "Welcome back, Inas! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const yahiya_amina: UserConfig = {
   id: "6521ddeb-023d-4a19-826a-e9effea4fa33",
   displayName: "Amina",
+  welcomeMessage: "Welcome back, Amina! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ikram_th: UserConfig = {
   id: "59a55d62-8aa0-4730-a114-035f25dfc3e8",
   displayName: "Ikram",
+  welcomeMessage: "Welcome back, Ikram! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const wafa_trg: UserConfig = {
   id: "5fd5aaf2-a5d2-41f9-9b01-4eae0ec9fb70",
   displayName: "Wafa",
+  welcomeMessage: "Welcome back, Wafa! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const chatri_yasmine: UserConfig = {
   id: "eb2e67e7-0009-4034-b938-94e5cd34b331",
   displayName: "Yasmine",
+  welcomeMessage: "Welcome back, Yasmine! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const chatri_yasmine_2: UserConfig = {
   id: "d405e914-aaf5-4099-b409-15bde0ac6897",
   displayName: "Yasmine",
+  welcomeMessage: "Welcome back, Yasmine! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const fodil_tahar_el_amine: UserConfig = {
   id: "fb3cc807-6613-41a0-aa68-a4f66d027bae",
   displayName: "Amine",
+  welcomeMessage: "Welcome back, Amine! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ikram_mil_2006: UserConfig = {
   id: "73966e68-a26e-4f8f-94ae-4d8d8b288d29",
   displayName: "Ikram",
+  welcomeMessage: "Welcome back, Ikram! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ikram_mil: UserConfig = {
   id: "3c69803d-64d3-4846-85c3-91f02acd4472",
-  displayName: "",
+  displayName: "Ikram",
+  welcomeMessage: "Welcome back, Ikram! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const yazid_rk: UserConfig = {
   id: "03326b83-04bd-4f92-a008-b188b50eeefe",
   displayName: "Yazid",
+  welcomeMessage: "Welcome back, Yazid! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const hayat: UserConfig = {
   id: "a1456c61-9e75-48c7-9d8d-548cd96756fb",
   displayName: "Hayat",
+  welcomeMessage: "Welcome back, Hayat! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const ranya: UserConfig = {
   id: "c83fa782-7710-4fb6-b372-7dd95e70729d",
   displayName: "Ranya",
+  welcomeMessage: "Welcome back, Ranya! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const azzouz_abdesattar_2: UserConfig = {
   id: "b22ced88-b91b-44f5-a88b-996f6433cc85",
   displayName: "Azzouz",
+  welcomeMessage: "Welcome back, Azzouz! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const blekecem_douaa: UserConfig = {
   id: "0e80b69c-045e-41eb-a6a2-810f5ee6008a",
   displayName: "Douaa",
+  welcomeMessage: "Welcome back, Douaa! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const yass_mh: UserConfig = {
   id: "6ae7f906-a1ef-4504-88e4-76a0e8363d02",
   displayName: "Yass",
+  welcomeMessage: "Welcome back, Yass! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const labbaci_hiba: UserConfig = {
   id: "756799b6-1805-47ae-991c-0b8a0d4762b4",
   displayName: "Hiba",
+  welcomeMessage: "Welcome back, Hiba! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const mahi_yazid: UserConfig = {
   id: "c1c60684-1162-4312-a677-180a147fb518",
   displayName: "Yazid",
+  welcomeMessage: "Welcome back, Yazid! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const gribi_karim: UserConfig = {
   id: "c81ca755-617d-4c25-9abf-e038c4501f0d",
   displayName: "Karim",
+  welcomeMessage: "Welcome back, Karim! Ready to study?",
+  showOnlyYears: ["l2"],
 };
 
 const medadd_imed: UserConfig = {
   id: "13536e56-0062-4b3c-9377-d1793f03aecf",
   displayName: "Imed",
+  welcomeMessage: "Welcome back, Imed! Ready to study?",
+  showOnlyYears: ["l2"],
+};
+
+const zakaria_hamimeche: UserConfig = {
+  id: "0a5d7a8d-f74f-4311-8fbd-7c5ab1334aaf",
+  displayName: "Zakaria",
+  welcomeMessage: "Welcome back, Zakaria! Ready to study?",
+  showOnlyYears: ["l2"],
+  customBanner: "E9ra ela roehek ya zaki",
 };
 
 
@@ -551,4 +732,5 @@ export const ALL_USERS_FROM_CONFIGS: UserConfig[] = [
   mahi_yazid,
   gribi_karim,
   medadd_imed,
+  zakaria_hamimeche,
 ];
